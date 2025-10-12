723726915de6472b48cfc43f6ba7406984a6e8dc
srikanta0427 <srikanta0427@gmail.com>
2025-10-13 01:08:15 +0530

    Add files via upload

---
+++ 1-String/12-join().py
@@ -0,0 +1,7 @@
+s1 = 'abc'
+s2 = 'xyz'
+s3 = s1.join(s2)
+s4 = s2.join(s1)
+print(s3)
+print(s4)
+print('/'.join(s1))
---
+++ 1-String/13-split().py
@@ -0,0 +1,9 @@
+text = 'python is easy'
+words = text.split()
+print(words)           #Output   ['python', 'is', 'easy']
+
+
+text = 'python,is,easy,language'
+words = text.split(',',2)
+print(words)        #Output     ['python', 'is', 'easy,language']
+
