d2cb55ed64c06428f78fd835a659b5140406f216
srikanta0427 <srikanta0427@gmail.com>
2025-10-20 15:37:05 +0530

    Add files via upload

---
+++ 03-list/2-traversing_list.py
@@ -0,0 +1,14 @@
+l = [34,56,56,78]
+
+for i in l:
+    print(i)
+
+print('------------------------------------------')
+for i in range(len(l)):
+    print(l[i]*1)
+
+print('------------------------------------------')
+var = 0
+while var<len(l):
+    print(l[var])
+    var+=1
