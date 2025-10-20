8f8c0caaa86ee5986cce7823f115075145b5819d
srikanta0427 <srikanta0427@gmail.com>
2025-10-20 15:36:31 +0530

    Create declaration.py

---
+++ 03-list/declaration.py
@@ -0,0 +1,15 @@
+l1 = list('abcd')
+print(l1)
+
+l2 = ['str',12]
+print(l2)
+
+l3 = list((4,5,6,6))
+print(l3[0]+l2[1])
+
+# list in list 
+
+li = [2,3,4,5]
+li[3] = [5,6,7,8]
+print(li)
+print(li[3][2])     # 7
