f59a7fcc5590c2fb3e435961fd9485811e62f0e2
srikanta0427 <srikanta0427@gmail.com>
2025-11-02 01:05:48 +0530

    Add files via upload

---
+++ 03-list/12-del.py
@@ -0,0 +1,14 @@
+# delete the variable or remove the reference 
+x = 10
+del x
+# print(x)
+
+# remove the item from list
+l1 = [2,4,6,8]
+del l1[3]
+print(l1)
+
+# delete a slice from the list
+l2 = [5,7,11,13,17,19,23,31]
+del l2[2:5]
+print(l2)
