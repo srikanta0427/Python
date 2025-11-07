7f495c91e5a7252f8e5d430beeebc50bd5cb3b10
srikanta0427 <srikanta0427@gmail.com>
2025-11-07 15:07:23 +0530

    Create declaration.py

---
+++ tuple/declaration.py
@@ -0,0 +1,24 @@
+t1 = (1,2,3,4)
+print(t1)
+print(t1[3])
+print(type(t1))
+
+t2 = tuple("string")
+print(t2)
+
+t3 = ()
+
+t4 = (5,)
+print(t4)
+
+t5 = tuple(range(1,11))
+print(t5)
+
+t6 = ([2,4,6,8])
+print(t6)
+
+t7 = 10,20,30,40
+print(type(t7))
+
+t7[2] = 300
+print(t7[2])
