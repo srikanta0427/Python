5226670464cca2380e58f005a5a55594fee1e803
srikanta0427 <srikanta0427@gmail.com>
2025-11-12 02:03:48 +0530

    Create declaration.py

---
+++ set/declaration.py
@@ -0,0 +1,16 @@
+s1 = {2,4,6,8,8,4}
+print(s1)
+
+s2 = set([2,4,6,8])
+print(s2)
+
+s3 = set("python")
+print(s3)
+
+s4 = set()  # empty set
+
+s5 = {}
+print(type(s5))     # this is not a set , this is dict
+
+s6 = {4}
+print(type(s6))
