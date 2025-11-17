831fda05c8e17a69ec0e75262be811ad2f351303
srikanta0427 <srikanta0427@gmail.com>
2025-11-18 01:43:18 +0530

    Add files via upload

---
+++ function/4-return.py
@@ -0,0 +1,7 @@
+def get_formatted_name(firstName, lastName):
+    return firstName.title()+' '+lastName.title()
+
+getName = get_formatted_name("srikanta","barik")
+print(getName)
+
+print(get_formatted_name("soumya","nanda"))
---
+++ function/5-passing_a_list.py
@@ -0,0 +1,7 @@
+new_list = ["srikanta","soumya","dibya","pankaj","anuj"]
+
+def Name(names):
+    for name in names:
+        print(f'{name.title()}')
+
+Name(new_list)
